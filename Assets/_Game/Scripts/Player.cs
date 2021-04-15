76360bd510c6fdbb096e7f857f0a4123b3c535e7
SergeSimak <sergesimak2012@gmail.com>
2021-04-15 20:46:44 +0300

    SR

---
+++ Assets/_Game/Scripts/Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+namespace Game
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] private Animator _animator;
+        [SerializeField] private List<Transform> _currentPath;
+
+        [SerializeField] private float _moveSpeed;
+        [SerializeField] private float _rotateSpeed;
+
+        [SerializeField] private int _countPathChunks;
+
+        private int _chunksOnPositions;
+
+        private void Awake()
+        {
+            RoadChunk.OnRightPositionSet += ChunkOnPosition;
+        }
+
+        private IEnumerator MoveThroughPath(float time)
+        {
+            while (_currentPath.Count > 0)
+            {
+                var currentPos = transform.position;
+                var directionToPoint = (_currentPath[0].position - currentPos).normalized;
+                var rotation = Quaternion.LookRotation(directionToPoint).eulerAngles;
+                rotation.x = 0;
+                rotation.z = 0;
+
+                float elapsedTime = 0;
+
+                while (elapsedTime < time)
+                {
+                    transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(rotation), _rotateSpeed * (elapsedTime / time));
+                    transform.position = Vector3.Lerp(currentPos, _currentPath[0].position, (elapsedTime / time));
+                    elapsedTime += Time.deltaTime;
+                    yield return null;
+                }
+
+                yield return time;
+                _currentPath.RemoveAt(0);
+            }
+
+            _animator.SetBool("Run", false);
+        }
+
+        public void ChunkOnPosition()
+        {
+            _chunksOnPositions++;
+
+            if (_chunksOnPositions == _countPathChunks)
+            {
+                _animator.SetBool("Run", true);
+                StartCoroutine(MoveThroughPath(1f));
+            }
+        }
+
+        private void OnDestroy()
+        {
+            RoadChunk.OnRightPositionSet -= ChunkOnPosition;
+        }
+    }
+}
---
+++ Assets/_Game/Scripts/RoadChunk.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using System;
+
+namespace Game
+{
+    public class RoadChunk : MonoBehaviour
+    {
+        public static Action OnRightPositionSet;
+
+        [SerializeField] private float _currentYRotation;
+        [SerializeField] private float _animateScaleTime;
+        [SerializeField] private bool _isRightChank;
+
+        private bool _inPosition;
+
+        private void Start()
+        {
+            CheckRightPosition();
+        }
+
+        private void OnMouseDown()
+        {
+            if (_inPosition)
+                return;
+
+            transform.rotation *= Quaternion.Euler(Vector3.up * 90);
+            CheckRightPosition();
+        }
+
+        private void CheckRightPosition()
+        {
+            if (_isRightChank && transform.rotation.eulerAngles.y == _currentYRotation)
+            {
+                _inPosition = true;
+                var startScale = transform.localScale;
+
+                OnRightPositionSet?.Invoke();
+
+                Sequence seq = DOTween.Sequence();
+                seq.Append(transform.DOScale(startScale * 1.25f, _animateScaleTime / 2f));
+                seq.Append(transform.DOScale(startScale, _animateScaleTime / 2f));
+            }
+        }
+    }
+
+}
